7e63e3fbbc20307accefe0f85bec9a3ba1017cb4
Michael Madsen <mm@pug-works.dk>
2020-06-02 20:50:05 +0200

    hidden files

---
+++ .scripts/.scripts/wallpaper_handler.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# feh --randomize --auto-zoom --geometry 1920x1080+0+0 --bg-fill ~/Nextcloud/Wallpapers/1080P &
+
+my_laptop_external_monitor=$(xrandr --query | grep 'DisplayPort-0')
+if [[ $my_laptop_external_monitor = *connected* ]]; then
+  feh --recursive --randomize --auto-zoom --geometry 2560x1440+1920+0 --bg-fill ~/Nextcloud/Wallpapers &
+else
+  feh --recursive --randomize --auto-zoom --geometry 1920x1080+0+0 --bg-fill ~/Nextcloud/Wallpapers &
+fi
